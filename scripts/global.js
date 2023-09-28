709fefc49752ae7f1745cdc887102fe3e11be2be
barbaraanascimento <145502981+barbaraanascimento@users.noreply.github.com>
2023-09-28 16:07:28 -0300

    Consertada a velocidade do pulo e o tamanho do canvas.

--- scripts/global.js
+++ scripts/global.js
@@ -23,45 +23,54 @@ const execFuncoes = ([primeiroElemento, ...resto]) => {
 /*  Aqui onde é criado o menu principal.
     Impressão da logo e mensagem. */
 const criarMenu = (ctx) => (logo) => (cenario) => {
-    // Define o tamanho do canvas para o mesmo tamanho da janela
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-
+    cenario.definirTamanho(ctx)(600)(300);
     const canvasTamanho = cenario.tamanho(ctx); // Tamanho do canvas
     const canvasCentro  = cenario.proporcao(1/2)(canvasTamanho); // Centro do canvas
-    const logoDimensoes = cenario.proporcao(1/3)({ x: logo.width, y: logo.height }); // Dimensões da imagem logo
+    const logoDimensoes = cenario.proporcao(1/(window.devicePixelRatio * 500))({ x: logo.width, y: logo.height }); // Dimensões da imagem logo
     const logoPosicao   = cenario.proporcao(1/2)(logoDimensoes); // Posição da imagem logo
+    // ctx.drawImage(logo, canvasCentro.x - logoPosicao.x, canvasCentro.y - logoPosicao.y, logoDimensoes.x, logoDimensoes.y); // Imprimir a logo
+    ctx.drawImage(logo, logoDimensoes.x, logoDimensoes.y, canvasTamanho.x, canvasTamanho.y);
 
-    ctx.drawImage(logo, canvasCentro.x - logoPosicao.x, canvasCentro.y - logoPosicao.y, logoDimensoes.x, logoDimensoes.y); // Imprimir a logo
-
-    const mensagemComecar = cenario.formatarTexto(ctx)(25)("center")("#000000"); // Criação do texto da tela inicial
-    mensagemComecar.fillText("Pressione ESPAÇO para começar a jogar", canvasCentro.x, canvasTamanho.y - 100);
     ctx.closePath();
 };
 
 // Criar fase: jogador, obstáculos, potuação
 const criarFase = (ctx) => (estado) => (cenario) => (jogador) => {
     if (estado.faseAtual < 1) return;
-    if (estado.obstaculos.length === 0) estado.obstaculos = Array(5).fill().map((valor, index) => cenario.criarObstaculo((estado.semente() + 1) * 200)(index)(window.innerWidth));
-    if (estado.habilidades.length === 0) estado.habilidades = Array(1).fill().map((valor, index) => cenario.criarObstaculo((estado.semente() + 1) * 500)(2)(window.innerWidth));
+
+    cenario.definirTamanho(ctx)(600)(300);
+
+    const pontuacao = estado.pontuacao.toString();
+    const pontuacaoNormalizada = pontuacao.substring(0, pontuacao.length - 1);
+
+    const preencherLista = (tamanho) => (exec) => Array(tamanho).fill().map((_v, index) => exec(index));
+    const removerElemento = (qnt) => (lista) => lista.slice(qnt);
+    const removerPrimeiroElemento = removerElemento(1);
+
+    const criarObstaculos = () => preencherLista(5)((index) => cenario.criarObstaculo(estado.semente() * 200)(index)(0));
+    const criarHabilidades = () => preencherLista(1)(() => cenario.criarObstaculo(estado.semente() * 500)(2)(0));
+
+    const estaForaDaTela = (posicao) => (tamanho) => posicao < 0 - tamanho; 
+
+    // estado.obstaculos = Array(5).fill().map((valor, index) => cenario.criarObstaculo((estado.semente() + 1) * 200)(index+1)(window.innerWidth));
+    if (estado.obstaculos.length === 0) estado.obstaculos = criarObstaculos();
+    if (estado.habilidades.length === 0) estado.habilidades = criarHabilidades();
     
     // Caso o primeiro obstáculo da lista esteja fora da tela, remover da lista e criar um novo
-    if (estado.obstaculos[0].x < 0) {
-        estado.obstaculos = estado.obstaculos.slice(1);
-        estado.obstaculos.push(cenario.criarObstaculo((estado.semente() + 1) * 200)(4)(0));
+    if (estaForaDaTela(estado.obstaculos[0].x)(estado.tamanhoInicialObstaculos.x)) {
+        estado.obstaculos = removerPrimeiroElemento(estado.obstaculos);
+        estado.obstaculos.push(cenario.criarObstaculo(estado.semente() * 200)(2)(-200));
     };
 
     // Caso a primeira habilidade da lista esteja fora da tela, remover da lista e criar uma nova
-    if (estado.habilidades[0].x < 0) {
-        estado.habilidades = estado.habilidades.slice(1);
+    if (estaForaDaTela(estado.habilidades[0].x)(estado.tamanhoInicialHabilidades.x)) {
+        estado.habilidades = removerPrimeiroElemento(estado.habilidades);
         estado.habilidades.push(cenario.criarObstaculo((estado.semente() + 1) * 200)(5)(0));
     }
-    // Define o tamanho do canvas
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
 
     const canvasTamanho = cenario.tamanho(ctx);
-
+    // if (Jogador.posicao.y === -1) Jogador.posicao.y = ctx.canvas.height / 2;
+ 
     // Mapeia os obstáculos para serem impressos e retorna uma nova posição para eles
     estado.obstaculos = estado.obstaculos.map((valor, index) => {
         cenario.desenharObstaculo(ctx)({ x: 25, y: 50 })({ x: valor.x * estado.velocidadeInicial, y: valor.y });
@@ -74,12 +83,11 @@ const criarFase = (ctx) => (estado) => (cenario) => (jogador) => {
         return { ...valor, x: valor.x - estado.velocidade };
     });
 
-    jogador.pular(125); // Define a altura do pulo
+    jogador.pular(estado.alturaPulo); // Define a altura do pulo
     jogador.desenhar(ctx)(jogador.posicao)(jogador.tamanho); // Imprimir jogador
-    
      // Preparar texto de pontuação
-    const pontuacao = cenario.formatarTexto(ctx)(16)("right")("black");
-    pontuacao.fillText(estado.pontuacao, canvasTamanho.x - 100, 100);
+    const exibirPontuacao = cenario.formatarTexto(ctx)(16)("right")("black");
+    exibirPontuacao.fillText(pontuacaoNormalizada, canvasTamanho.x, 10);
     ctx.closePath();
 };
 
@@ -103,7 +111,6 @@ window.addEventListener("resize", () => {
     Estado.limparObstaculos();
 }, false);
 
-<<<<<<< HEAD
 /*
 const checagemMorte = setInterval(() =>{
 
@@ -133,8 +140,6 @@ const checagemMorte = setInterval(() =>{
 //Essa parte de criar a colisão eu entendi como faz e tals, mas eu preciso de ajuda para fazer se encaixar nas arrays.
 //queria saber como faz, para conectar isso com o restante do código.
 
-const loopJogo = (tempo) => {
-=======
 /* -------------------------------------------------------------------------- */
 /*                                    LOOP                                    */
 /* -------------------------------------------------------------------------- */
@@ -148,7 +153,6 @@ const JOGO = [ () => criarFase(ctx)(Estado)(Cenario)(Jogador) ];
 const loopJogo = (executar) => (listaFuncoes) => (tempoAtual) => (tempo) => {
     /*  ** Infelizmente, até o momento não encontramos uma forma de gerenciar estados sem quebrar o paradigma funcional.
         Seguiremos buscando */
->>>>>>> 5a3c0e5701365aa52351355e609973c135207be7
     if (Estado.faseAtual == -1) Estado.faseAtual = 0;
     if (Estado.faseAtual > 0) Estado.pontuacao += 1;
 
